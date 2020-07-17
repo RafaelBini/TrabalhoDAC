3a916a45625f5cc4b7b95850fdd285621e24237d
yokuronai <rfabini1996@gmail.com>
2020-07-17 09:22:16 -0300

    Corrigi problemas de navegação no SOSIFOD

--- SOSIFOD/src/java/mb/IntimacaoMB.java
+++ SOSIFOD/src/java/mb/IntimacaoMB.java
@@ -251,6 +251,12 @@ public class IntimacaoMB implements Serializable {
         conversation.end();
         return "intimacoes";
     }
+    
+    public String voltArdmin() {
+        conversation.end();
+        return "admin";
+    }
+    
 
     public String cadastrar() throws Exception {
         try {
